ee00b3bc2c587430bac3a66571e9f17f23c5cc48
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2021-01-11 13:49:38 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+  
+##################################################################################################################################
+# Deployment
+# ----------
+
+echo PHP deployment
+
+# 1. KuduSync
+if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
+  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
+  exitWithMessageOnError "Kudu Sync failed"
+fi
+
+# 3. Initialize Composer Config
+initializeDeploymentConfig
+
+# 4. Use composer
+echo "$DEPLOYMENT_TARGET"
+if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
+  echo "Found composer.json"
+  pushd "$DEPLOYMENT_TARGET"
+  php composer.phar install $COMPOSER_ARGS
+  exitWithMessageOnError "Composer install failed"
+  popd
+fi
+##################################################################################################################################
