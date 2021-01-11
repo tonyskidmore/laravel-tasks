be2296a845df249378c988c0edb4f6786f2d1b81
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2021-01-11 14:06:19 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,82 @@
-  
+#!/bin/bash
+
+# ----------------------
+# KUDU Deployment Script
+# Version: 1.0.17
+# ----------------------
+
+# Helpers
+# -------
+
+exitWithMessageOnError() {
+  if [ ! $? -eq 0 ]; then
+    echo "An error has occurred during web site deployment."
+    echo $1
+    exit 1
+  fi
+}
+
+# Prerequisites
+# -------------
+
+# Verify node.js installed
+hash node 2>/dev/null
+exitWithMessageOnError "Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment."
+
+# Setup
+# -----
+
+SCRIPT_DIR="${BASH_SOURCE[0]%\\*}"
+SCRIPT_DIR="${SCRIPT_DIR%/*}"
+ARTIFACTS=$SCRIPT_DIR/../artifacts
+KUDU_SYNC_CMD=${KUDU_SYNC_CMD//\"/}
+
+if [[ ! -n "$DEPLOYMENT_SOURCE" ]]; then
+  DEPLOYMENT_SOURCE=$SCRIPT_DIR
+fi
+
+if [[ ! -n "$NEXT_MANIFEST_PATH" ]]; then
+  NEXT_MANIFEST_PATH=$ARTIFACTS/manifest
+
+  if [[ ! -n "$PREVIOUS_MANIFEST_PATH" ]]; then
+    PREVIOUS_MANIFEST_PATH=$NEXT_MANIFEST_PATH
+  fi
+fi
+
+if [[ ! -n "$DEPLOYMENT_TARGET" ]]; then
+  DEPLOYMENT_TARGET=$ARTIFACTS/wwwroot
+else
+  KUDU_SERVICE=true
+fi
+
+if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
+  # Install kudu sync
+  echo Installing Kudu Sync
+  npm install kudusync -g --silent
+  exitWithMessageOnError "npm failed"
+
+  if [[ ! -n "$KUDU_SERVICE" ]]; then
+    # In case we are running locally this is the correct location of kuduSync
+    KUDU_SYNC_CMD=kuduSync
+  else
+    # In case we are running on kudu service this is the correct location of kuduSync
+    KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
+  fi
+fi
+
+# PHP Helpers
+# -----------
+
+initializeDeploymentConfig() {
+  if [ ! -e "$COMPOSER_ARGS" ]; then
+    COMPOSER_ARGS="--no-progress --no-dev --verbose"
+    echo "No COMPOSER_ARGS variable declared in App Settings, using the default settings"
+  else
+    echo "Using COMPOSER_ARGS variable declared in App Setting"
+  fi
+  echo "Composer settings: $COMPOSER_ARGS"
+}
+
 ##################################################################################################################################
 # Deployment
 # ----------
@@ -11,6 +89,10 @@ if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
   exitWithMessageOnError "Kudu Sync failed"
 fi
 
+# 2. Verify composer installed
+#hash composer 2>/dev/null
+#exitWithMessageOnError "Missing composer executable"
+
 # 3. Initialize Composer Config
 initializeDeploymentConfig
 
@@ -18,9 +100,33 @@ initializeDeploymentConfig
 echo "$DEPLOYMENT_TARGET"
 if [ -e "$DEPLOYMENT_TARGET/composer.json" ]; then
   echo "Found composer.json"
+  # shellcheck disable=SC2164
   pushd "$DEPLOYMENT_TARGET"
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php
+  php -r "unlink('composer-setup.php');"
   php composer.phar install $COMPOSER_ARGS
   exitWithMessageOnError "Composer install failed"
+  # shellcheck disable=SC2164
   popd
 fi
+
+echo "Laravel deployment"
+
+# shellcheck disable=SC2164
+pushd "$DEPLOYMENT_TARGET"
+curl https://kpas-lti.azurewebsites.net/api/command/migrate
+echo -e "\n"
+php artisan cache:clear
+php artisan route:clear
+php artisan route:cache
+php artisan config:clear
+php artisan config:cache
+exitWithMessageOnError "Laravel deploy failed"
+# shellcheck disable=SC2164
+popd
+
+
 ##################################################################################################################################
+echo "Finished successfully."
